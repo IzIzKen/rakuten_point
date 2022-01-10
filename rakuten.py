49ac6a5932af4908ba7912ba4db72260496bc4b4
IzIzKen <111420knt@gmail.com>
2022-01-10 15:15:33 +0900

    add start lotteries function

--- rakuten.py
+++ rakuten.py
@@ -1,4 +1,6 @@
 from selenium import webdriver
+from selenium.webdriver.chrome import service as fs
+from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 import time
@@ -7,20 +9,21 @@ import csv
 # chromeDriverの設定
 chromeOptions = webdriver.ChromeOptions()
 chromeOptions.add_argument('--window-size=1920,1080')
-chromeOptions.add_argument('--headless')
+# chromeOptions.add_argument('--headless')
 chromeDriver = "chromedriver.exe"
-driver = webdriver.Chrome(executable_path=chromeDriver, options=chromeOptions)
+chrome_service = fs.Service(executable_path=chromeDriver)
+driver = webdriver.Chrome(service=chrome_service)
 wait = WebDriverWait(driver=driver, timeout=60)
 # ユーザー情報の設定
-USERID = ''
-PASSWORD = ''
+USERID = '111420knt@gmail.com'
+PASSWORD = '111420knt'
 
 driver.get('https://rakucoin.appspot.com/rakuten/kuji/')
 wait.until(EC.presence_of_element_located)
-time.sleep(3)
-error_flag = False
+time.sleep(1)
+login_flag = False
 
-lotteries = driver.find_elements_by_tag_name('a')
+lotteries = driver.find_elements(By.TAG_NAME, 'a')
 
 outputs = []
 
@@ -28,25 +31,26 @@ for lottery in lotteries:
     lottery.click()
     wait.until(EC.presence_of_element_located)
     time.sleep(3)
-    if error_flag is False:
-        try:
-            username_input = driver.find_element_by_xpath('//input[@name="u"]')
-            print("a")
-            username_input.send_keys(USERID)
-            time.sleep(1)
+    driver.switch_to.window(driver.window_handles[1])
 
-            password_input = driver.find_element_by_xpath('//input[@name="p"]')
-            password_input.send_keys(PASSWORD)
-            time.sleep(1)
+    if login_flag is False:
+        driver.find_element(By.ID, 'loginInner_u').send_keys(USERID)
+        driver.find_element(By.ID, 'loginInner_p').send_keys(PASSWORD)
+        driver.find_element(By.CLASS_NAME, 'loginButton').click()
+        wait.until(EC.presence_of_element_located)
+        time.sleep(3)
+        login_flag = True
 
-            username_input.submit()
-            time.sleep(1)
+    if len(driver.find_elements(By.ID, 'entry')) > 0:
+        driver.find_element(By.ID, 'entry').click()
+        wait.until(EC.presence_of_element_located)
+        time.sleep(15)
+    #    くじを引くidがentry以外の場合どうするか？
 
-        except Exception:
-            error_flg = True
-            print('ユーザー名、パスワード入力時にエラーが発生しました。')
+    driver.close()
+    time.sleep(1)
 
-    break
+    driver.switch_to.window(driver.window_handles[0])
 
 driver.close()
 driver.quit()
