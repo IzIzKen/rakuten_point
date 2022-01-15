5f1bed541a5e5d27df6b564939d9b2f62f938b6e
IzIzKen <111420knt@gmail.com>
2022-01-15 16:07:59 +0900

    add start all lottery

--- rakuten.py
+++ rakuten.py
@@ -4,7 +4,6 @@ from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 import time
-import csv
 
 # chromeDriverの設定
 chromeOptions = webdriver.ChromeOptions()
@@ -12,7 +11,7 @@ chromeOptions.add_argument('--window-size=1920,1080')
 # chromeOptions.add_argument('--headless')
 chromeDriver = "chromedriver.exe"
 chrome_service = fs.Service(executable_path=chromeDriver)
-driver = webdriver.Chrome(service=chrome_service)
+driver = webdriver.Chrome(service=chrome_service, options=chromeOptions)
 wait = WebDriverWait(driver=driver, timeout=60)
 # ユーザー情報の設定
 USERID = '111420knt@gmail.com'
@@ -21,10 +20,9 @@ PASSWORD = '111420knt'
 driver.get('https://rakucoin.appspot.com/rakuten/kuji/')
 wait.until(EC.presence_of_element_located)
 time.sleep(1)
-login_flag = False
 
 lotteries = driver.find_elements(By.TAG_NAME, 'a')
-
+login_flag = True
 outputs = []
 
 for lottery in lotteries:
@@ -33,18 +31,18 @@ for lottery in lotteries:
     time.sleep(3)
     driver.switch_to.window(driver.window_handles[1])
 
-    if login_flag is False:
+    if login_flag:
         driver.find_element(By.ID, 'loginInner_u').send_keys(USERID)
         driver.find_element(By.ID, 'loginInner_p').send_keys(PASSWORD)
         driver.find_element(By.CLASS_NAME, 'loginButton').click()
         wait.until(EC.presence_of_element_located)
         time.sleep(3)
-        login_flag = True
+        login_flag = False
 
     if len(driver.find_elements(By.ID, 'entry')) > 0:
         driver.find_element(By.ID, 'entry').click()
         wait.until(EC.presence_of_element_located)
-        time.sleep(15)
+        time.sleep(20)
     #    くじを引くidがentry以外の場合どうするか？
 
     driver.close()
@@ -54,8 +52,3 @@ for lottery in lotteries:
 
 driver.close()
 driver.quit()
-
-# # shift-jis, utf8, cp932
-# with open(f"outputs.csv", "a", newline="", encoding="utf8") as f:
-#     writer = csv.writer(f)
-#     writer.writerows(outputs)
